63486396e78c4fd520e9f8dba74fa986139596db
denys <denys.kozii@gmail.com>
2022-08-01 17:54:10 +0300

    comment scheduler

--- src/main/java/com/incheck/api/utils/Scheduler.java
+++ src/main/java/com/incheck/api/utils/Scheduler.java
@@ -1,18 +1,11 @@
 package com.incheck.api.utils;
 
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
-
-import java.io.IOException;
 
 @Data
 @Component
@@ -20,14 +13,14 @@ import java.io.IOException;
 @EnableScheduling
 public class Scheduler {
 
-    @Value("${scheduler-url}")
-    private String URL;
-
-    @Scheduled(fixedRate = 1000 * 60 * 5)
-    public void timer() throws IOException {
-        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
-            HttpUriRequest request = new HttpGet(URL);
-            client.execute(request);
-        }
-    }
+//    @Value("${scheduler-url}")
+//    private String URL;
+//
+//    @Scheduled(fixedRate = 1000 * 60 * 5)
+//    public void timer() {
+//        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+//            HttpUriRequest request = new HttpGet(URL);
+//            client.execute(request);
+//        }
+//    }
 }
